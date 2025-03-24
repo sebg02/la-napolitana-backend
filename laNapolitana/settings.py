1749ecc59545594bb79cbf12a2e56d6219219ce8
SEB <sebasgr00@gmail.com>
2025-03-23 21:52:21 -0500

    correct other typo

--- laNapolitana/settings.py
+++ laNapolitana/settings.py
@@ -78,14 +78,12 @@ WSGI_APPLICATION = 'laNapolitana.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        dj_database_url.config(
+    'default': dj_database_url.config(
             default="postgresql://postgres:postgres@localhost:5432/mysite",
             conn_max_age=600
         )
-    }
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 }
 
 
